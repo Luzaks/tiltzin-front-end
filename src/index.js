1610bddb30dca335719111b8591374b9b811746c
Luzaks <kanemekanik@gmail.com>
2020-08-04 13:40:34 -0500

    Fix: Change worng importing file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BrowserRouter from 'react-router-dom/modules/BrowserRouter';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './styles/index.css';
 import store from './Redux/store/store';

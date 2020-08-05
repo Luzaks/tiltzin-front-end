1cdb1249cf02afdf5b6f890915c7451db4344627
Luzaks <kanemekanik@gmail.com>
2020-08-05 14:40:12 -0500

    Feat: Upgrade Redux state.

--- src/Redux/reducers/loggedInReducer.js
+++ src/Redux/reducers/loggedInReducer.js
@@ -1,6 +1,7 @@
 import { LOGGED_USER } from '../actions/actionTypes';
 
 const loggedIn = (state = [], action) => {
+  console.log(action);
   if (action.type === LOGGED_USER) return action.payload;
   return state;
 };
--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import Routes from '../Router/Routes';
+import { logginCreator } from '../Redux/actions/actions';
 import '../styles/App.css';
 
 const mapStateToProps = state => ({ loggedIn: state.loggedIn });
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addLog: loggedIn => dispatch(logginCreator(loggedIn)),
+  };
+}
 
 class App extends React.Component {
 
   checkLoginStatus() {
     axios.get("http://localhost:3001/logged_in", { withCredentials: true })
-      .then( r => { console.log(r) })
+      .then( r => { 
+        if(r.data.logged_in && this.props.loggedIn.status === 'NOT_LOGGED_IN') {
+          const {addLog} = this.props;
+          addLog(r.data);
+        }
+      })
       .catch(error => { console.log(error) });
   }
 
@@ -37,4 +49,4 @@ App.propTypes = {
   loggedIn: PropTypes.objectOf(PropTypes.any).isRequired,
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);

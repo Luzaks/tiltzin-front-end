1670506fae59bcbadaccd737409f9be1d63a7182
Luzaks <kanemekanik@gmail.com>
2020-08-11 12:25:29 -0500

    Test: Add tests for App component.

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow, configure } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../components/App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/app/i);
-  expect(linkElement).toBeInTheDocument();
+const initialState = {
+  user: {
+    username: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  },
+  loggedIn: {
+    status: 'NOT_LOGGED_IN',
+    user: {},
+  },
+  pages: {
+    clicked: 'HOME',
+    pages: ['HOME', 'DESTINIES', 'APPOINTMENTS'],
+  },
+  destinies: [],
+  selected: {
+    active: false,
+    destiny: {},
+  },
+  trip: {
+    date: '',
+  },
+  trips: [],
+};
+const mockStore = configureMockStore();
+const store = mockStore({initialState});
+configure({adapter: new Adapter()});
+
+describe("App Component", () => {
+  it("should render without throwing an error", () => {
+      expect(
+          shallow(
+              <Provider store={store}>
+                  <App />
+              </Provider>
+          ).exists(<span class="sc-AxhUy hdSVIO">tiltzin</span>)
+      ).toBe(false);
+  });
 });
